ef07f150012df5759ae4db89b772a1006c756982
rtownsend722 <rebecca.townsend722@gmail.com>
2017-10-20 20:22:30 -0700

    Populates data to screen

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,7 +6,13 @@ app.URL = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
 
 app.init = function() {
   $('#main').find('.username').on('click', this.handleUsernameClick);
-  $('#send').find('.submit').unbind('submit').bind('submit', () => { this.handleSubmit(); } );
+  // $('#send').find('.submit').unbind('submit').bind('submit', () => { this.handleSubmit(); } );
+  $('#send').find('.submit').on('click', (event) => { 
+    this.handleSubmit(); 
+    // event.preventDefault();
+  });
+
+  this.fetch(this.postMessages);
 };
 
 app.send = function(message) { 
@@ -17,22 +23,26 @@ app.send = function(message) {
   }); 
 };
 
-app.fetch = function() {
+app.fetch = function(cb) {
   $.ajax({
     url: this.URL,
     type: 'GET',
     crossDomain: true,
-    success: function(data) {console.log("data", data); console.log('success!');},
-    error: function() {console.log('error');}
+    success: function (data) {
+      cb(data);
+    },
+    error: function() { console.log('error'); }
   });
+
 };
+
 
 app.clearMessages = function() {
   $('#chats').children().remove();
 };
 
 app.renderMessage = function(message) {
-  $('#chats').append(`<div class="chat"><span class="username">${message.username}</span><span>${message.text}</span></div>`);
+  $('#chats').append(`<div class="chat"><span class="username">${message.username}</span><span>${message.text}</span><span class="roomname">${message.roomname}</span></div>`);
 
 };
 
@@ -45,13 +55,44 @@ app.handleUsernameClick = function() {
 };
 
 app.handleSubmit = function() {
-  let data = $('#send').find('#message');
+
+  console.log('TEST');
+
+  let data = $('#send').find('#message').val();
 
   this.send(data);
+
+  // also update our own component
+  // get data from form
+  const message = $('#message').val();
+  console.log(message);
+  this.renderMessage({username: 'self', message: message, roomname: 'somethingdistinctive'});
 };
 
-app.fetch();
+app.escapeMessage = function(obj) {
+  let {text, username, roomname} = obj;
 
+  text = escape(text);
+  username = escape(username);
+  roomname = escape(roomname);
+
+  return {text, username, roomname};
+};
+
+app.postMessages = function(data) {
+  data = data.results;
+
+  for (let i = 0; i < data.length; i++) {
+    const thisMessage = data[i];
+    const safeMessage = app.escapeMessage(thisMessage);
+    app.renderMessage(safeMessage);
+  }
+
+};
+
+$(document).ready(function() {
+  app.init();
+});
 
 
 
