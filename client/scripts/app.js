2755554226ead12f8f933ce7b472cc853bc47751
rtownsend722 <rebecca.townsend722@gmail.com>
2017-10-20 17:33:21 -0700

    All tests pass and fetch retrieves data

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,58 @@
 // YOUR CODE HERE:
+
+var app = {};
+
+app.URL = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+
+app.init = function() {
+  $('#main').find('.username').on('click', this.handleUsernameClick);
+  $('#send').find('.submit').unbind('submit').bind('submit', () => { this.handleSubmit(); } );
+};
+
+app.send = function(message) { 
+  $.ajax({
+    url: this.URL,
+    type: 'POST',
+    data: JSON.stringify(message),
+  }); 
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: this.URL,
+    type: 'GET',
+    crossDomain: true,
+    success: function(data) {console.log("data", data); console.log('success!');},
+    error: function() {console.log('error');}
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').children().remove();
+};
+
+app.renderMessage = function(message) {
+  $('#chats').append(`<div class="chat"><span class="username">${message.username}</span><span>${message.text}</span></div>`);
+
+};
+
+app.renderRoom = function(room) {
+  $('#roomSelect').append('<div>`${room}`</div>');
+};
+
+app.handleUsernameClick = function() {
+
+};
+
+app.handleSubmit = function() {
+  let data = $('#send').find('#message');
+
+  this.send(data);
+};
+
+app.fetch();
+
+
+
+
+
